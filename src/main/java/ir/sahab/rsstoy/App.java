ec7d736ecd1edaeaa06165ac83429c6e697a31b1
mousavi <mousavihosseini@ce.sharif.edu>
2018-07-07 14:43:09 +0430

    Initial Commit

---
+++ src/main/java/ir/sahab/rsstoy/App.java
@@ -0,0 +1,13 @@
+package ir.sahab.rsstoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
